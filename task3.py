b9e2d60f46b2e7966eb4964af1ea5eb323bc9d38
Natasha <71723641+ranatasha@users.noreply.github.com>
2022-09-02 11:45:19 +0300

    Create task3.py

---
+++ task3.py
@@ -0,0 +1,39 @@
+n, m, a = list(map(int, input().split()))
+my_list = []
+sm = 0
+x = 0
+y = 0
+b = 1
+c = 1
+d = 1
+e = 1
+for g in range(n):
+    my_list.append(list(map(int, input().split())))
+for i in range(n):
+    for j in range(m):
+        if my_list[i][j] == a:
+            x = i
+            y = j
+if y == 0:
+    if x == 0:
+        b = my_list[x + 1][y + 1]
+    elif x == m - 1:
+        b = my_list[x - 1][y + 1]
+    else:
+        b = my_list[x + 1][y + 1]
+        c = my_list[x - 1][y + 1]
+elif y == n - 1:
+    if x == 0:
+        b = my_list[x + 1][y - 1]
+    elif x == m - 1:
+        b = my_list[x - 1][y - 1]
+    else:
+        b = my_list[x + 1][y - 1]
+        c = my_list[x - 1][y - 1]
+else:
+    if x != 0 and x != m - 1:
+        b = my_list[x + 1][y + 1]
+        c = my_list[x - 1][y + 1]
+        d = my_list[x + 1][y - 1]
+        e = my_list[x - 1][y - 1]
+print(b * c * d * e)
